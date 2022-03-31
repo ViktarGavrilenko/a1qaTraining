63aada46898298bc5e1b062520a77765e6449aef
Viktor Gavrilenko <viktargavrilenko@gmail.com>
2022-03-31 22:10:24 +0300

    added line with a logger message

--- src/main/java/pageobject/MainPage.java
+++ src/main/java/pageobject/MainPage.java
@@ -102,6 +102,7 @@ public class MainPage extends Form {
                 }
             }
         }
+        Logger.getInstance().info("Game ended: '" + finalMessage);
         return finalMessage;
     }
 
